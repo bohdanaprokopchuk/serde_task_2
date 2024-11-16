0c125becf83c32ecf4c2563615cc3e3f7f1c8389
Bohdana Prokopchuk <b.prokopchuk@ukma.edu.ua>
2024-11-16 03:22:44 +0200

    first commit

---
+++ target/debug/deps/serde_practice_task_2-f234436eef2780f3.d
@@ -0,0 +1,5 @@
+C:\Users\Dana\rust-learning-path\serde_practice_task_2\target\debug\deps\serde_practice_task_2-f234436eef2780f3.exe: src/main.rs
+
+C:\Users\Dana\rust-learning-path\serde_practice_task_2\target\debug\deps\serde_practice_task_2-f234436eef2780f3.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/serde_practice_task_2.d
@@ -0,0 +1,1 @@
+C:\Users\Dana\rust-learning-path\serde_practice_task_2\target\debug\serde_practice_task_2.exe: C:\Users\Dana\rust-learning-path\serde_practice_task_2\src\main.rs
